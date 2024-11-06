0202e625b9bb6ee3ecdab5868bc50a2ba47f77cb
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2024-11-06 12:44:36 -0500

    Finished CRUD functions and setup error handling

--- server.js
+++ server.js
@@ -28,10 +28,15 @@ app.get('/api/v1/jobs', (req, res) => {
     res.status(200).json({ jobs });
 });
 
-// app.post('/', (req, res) => {
-//     console.log(req);
-//     res.json({ message: 'Data Recieved', data: req.body});
-// });
+// GET SINGLE JOB
+app.get('/api/v1/jobs/:id', (req, res) => {
+    const { id } = req.params;
+    const job = jobs.find((job) => job.id === id);
+    if (!job) {
+      return res.status(404).json({ msg: `no job with id ${id}` });
+    }
+    res.status(200).json({ job });
+  });
 
 // CREATE JOB
 app.post('/api/v1/jobs', (req, res) => {
@@ -46,9 +51,47 @@ app.post('/api/v1/jobs', (req, res) => {
     res.status(200).json({ job });
 });
 
+// EDIT JOB
+app.patch('/api/v1/jobs/:id', (req, res) => {
+    const { company, position } = req.body;
+    if (!company || !position ) {
+        return res.status(400).json({ msg: 'please provide company and position' });
+    }
+    const { id } = req.params;
+    const job = jobs.find((job) => job.id === id);
+    if(!job) {
+        return res.status(404).json({ msg: `No job with id ${id}` });
+    }
+    
+    job.company = company;
+    job.position = position;
+    res.status(200).json({ msg: 'job modified', job });
+});
 
+// DELETE JOB
+app.delete('/api/v1/jobs/:id', (req, res) => {
+    const { id } = req.params;
+    const job = jobs.find((job) => job.id == id);
+    if(!job) {
+        return res.status(404).json({ msg: `No job with id ${id}` });
+    }
+    const newJobs = jobs.filter((job) => job.id !== id);
+    jobs = newJobs;
+    res.status(200).json({ msg: 'Job deleted' });
+});
 
+app.use('*', (req, res) => {
+    res.status(404).json({msg: 'Not Found'});
+});
 
+app.use('*', (err, req, res, next) => {
+    res.status(500).json({ msg:'something went wrong...'});
+});
+
+// app.post('/', (req, res) => {
+//     console.log(req);
+//     res.json({ message: 'Data Recieved', data: req.body});
+// });
 
 const port = process.env.PORT || 5100;
 app.listen(port, () => {
